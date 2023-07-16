dc7b56e0690c372403cd0f9a211acee24d670c7f
azandrew-sidoine <azandrewdevelopper@gmail.com>
2023-07-16 19:36:40 +0000

    upgraded angular version to v15

---
+++ scripts/version.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+while getopts u:a:f: flag
+do
+    case "${flag}" in
+        f) dir=${OPTARG};;
+    esac
+done
+
+arg=$1
+version=($(echo ${arg:=patch}))
+cwd=($(pwd))
+current_dir="${cwd}/projects/ngx-azl-cache"
+
+echo $current_dir
+cd "$current_dir"
+tag=$(npm version $version)
+git commit -a -m "version $tag release"
